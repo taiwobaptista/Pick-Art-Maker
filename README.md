95017a750206cd1aeb85b3f596bf0c1cbd039605
taiwo baptista <davidbaptista25@gmail.com>
2022-01-12 14:57:28 +0100

    My Pick0-Art-Picker

---
+++ README.md
@@ -0,0 +1,5 @@
+## Pixel Art Maker Project#
+
+This is my Javascript Project in the udacity-intro-to-programming nanodegree project to create a pixel Art maker App
+
+
---
+++ designs.js
@@ -0,0 +1,48 @@
+//  Select form
+const form = document.querySelector(".form");
+
+function makeGrid() {
+  // assign the users choice to variables. height, width and color
+  let height = document.getElementById("inputHeight").value;
+  let width = document.querySelector("#inputWidth").value;
+  let colorPicker = document.querySelector("#colorPicker");
+  //  Select the html table element
+  let table = document.querySelector("#pixelCanvas");
+
+  // clear the table cell
+  table.innerHTML = "";
+
+  let gridcolor = colorPicker.value;
+  // set the color  to the color a user choose
+  colorPicker.addEventListener("input", function () {
+    gridcolor = colorPicker.value;
+  });
+
+  // creating a table row
+  for (let i = 0; i <= height; i++) {
+    tableRow = table.insertRow(i);
+
+    // putting a table cells to each table row
+    for (let i = 0; i < width; i++) {
+      let tableCol = document.createElement("td");
+      tableCol.className = "columns";
+
+      tableRow.appendChild(tableCol);
+    }
+  }
+  const rows = document.querySelectorAll(".columns");
+  // insert color to the grid columns.
+  for (let i = 0; i < rows.length; i++) {
+    rows[i].addEventListener("click", function () {
+      rows[i].style.backgroundColor = gridcolor;
+    });
+  }
+}
+// added event listener to the form.
+
+form.addEventListener("submit", (e) => {
+  // preventing the form default behavour
+  e.preventDefault();
+  //create a table
+  makeGrid();
+});
